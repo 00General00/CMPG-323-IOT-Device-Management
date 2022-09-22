2b05d8c32c5ac995a30dedfabd8e879c65bb4b01
00General00 <generalntjie@gmail.com>
2022-09-22 23:47:59 +0200

    Created Repo and then added GetAll function

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class CategoryRepository
+    {
+        protected readonly ConnectedOfficeContext _connectedOfficeContext = new ConnectedOfficeContext();
+
+        // GET ALL: Category
+        public IEnumerable<Category> GetAll()
+        {
+            return _connectedOfficeContext.Category.ToList();
+
+        }
+        // TO DO: Add ‘Get By Id’
+        public IEnumerable<Category> GetId()
+        {
+            return _connectedOfficeContext.Category.ToList();
+
+        }
+        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Edit’
+        // TO DO: Add ‘Delete’
+        // TO DO: Add ‘Exists’
+
+
+    }
+}
