8ab7508e393abdbf421bdcbac93b17a47abee454
00General00 <generalntjie@gmail.com>
2022-09-23 02:44:21 +0200

    created Repository, interface and class

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+using DeviceManagement_WebApp.Models;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        void Remove(T Category);
+        void Add(T Category);
+
+        IEnumerable<T> GetAll();
+
+        //existing
+        Category Update(Category category);
+
+        void AddRange(IEnumerable<T> entities);
+        
+        void RemoveRange(IEnumerable<T> entities);
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    }
+
+}
