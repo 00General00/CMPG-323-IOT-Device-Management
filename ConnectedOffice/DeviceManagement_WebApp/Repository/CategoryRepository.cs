71f8334690839398d9c764fe975128add69a6405
00General00 <generalntjie@gmail.com>
2022-09-29 05:44:23 +0200

    Restarted Category Repo because of error in the previous one

--- ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -1,25 +1,65 @@
 ﻿using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DeviceManagement_WebApp.Repository
 {
-
     public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
     {
+        private ConnectedOfficeContext context;
+
         public CategoryRepository(ConnectedOfficeContext context) : base(context)
         {
+
         }
 
-        public Category GetMostRecentService()
+       
+
+
+        public async Task<Category> DeleteCategory(int id)
+        {
+            Category category = context.Category.Find(id);
+            context.Category.Remove(category);
+
+            return _context.Category.Find(id);
+        }
+
+        public void AddCategory(Category category)
+        {
+            _context.Category.Add(category);
+        }
+
+        public Category GetMostRecentCategories()
         {
             return _context.Category.OrderByDescending(Category => Category.DateCreated).FirstOrDefault();
         }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            _context.Entry(category).State = EntityState.Modified;
+        }
+
+
+        void ICategoryRepository.DeleteCategory(int id)
+        {
+            _context.Category.Find(id);
+            _context.Remove(id);
+
+        }
+
+        public Category GetCategoryById(Guid? categoryId)
+        {
+
+            return _context.Category.Find(categoryId);
+
+        }
     }
-
-
-
 }
